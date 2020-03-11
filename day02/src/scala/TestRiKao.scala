ed836104c1f6652036d97f6599e818b407ddc74b
Koujiao-my <2100313167@qq.com>
2020-03-11 10:29:01 +0800

    day02

---
+++ day02/src/scala/TestRiKao.scala
@@ -0,0 +1,16 @@
+object TestRiKao {
+  def main(args: Array[String]): Unit = {
+    //循环输出1-30
+    /*for (i <- 0 to 30) println(i)
+
+    //偶数输出
+    for(j <- 0 to 30)if (j%2==0)println(j)*/
+
+    var o :Int =0
+
+    while(o<50){
+      o+=1
+      println(o)
+    }
+  }
+}
---
+++ day02/src/scala/TestRiKao2.scala
@@ -0,0 +1,37 @@
+object TestRiKao2 {
+  def main(args: Array[String]): Unit = {
+      /*var a : Int =0
+      while(a<50){
+        a+=1
+        if (a%7==0){
+          println(a)
+        }
+      }*/
+
+    /*val list1 = List(1,2,3,"abc")
+    val list5 =4 :: 5 :: 6 :: list1 :::Nil
+    println(list5)*/
+
+    /*val list1 = List(1,2,3,"abc")
+    val list5 =4 :: 5 :: 6 :: list1 ::: list1 :::Nil
+    println(list5)*/
+
+
+    /*val names= List("Alice","Bob","Nick")
+    val list = names.map(_.toUpperCase())
+    println(list)*/
+
+    //val t1=("1","a","b",true,"c")
+    //println(t1._4)
+    //println(t1.productElement(4))
+
+    /*for (item <- t1.productIterator){
+      println(item)
+    }*/
+    val bb=new Array[Int](10)
+    bb(4)=100
+    println(bb(4))
+
+
+  }
+}
